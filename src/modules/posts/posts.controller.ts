7deac411e95f8a4de41e93a05fe047be8cb7da7b
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2022-11-22 10:37:06 +0500

    Crud is ready

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { use } from 'passport';
 import { GetUser } from '../auth/decorator';
+import { CreatePostDto, UpdatePostDto } from './dto';
+import { PostsService } from './posts.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('posts')
 export class PostsController {
-    @UseGuards(AuthGuard('jwt'))
+    constructor(private postService: PostsService){}
+
     @Get()
     getAll(@GetUser('id') userId){
-        return "All posts by ID: " + userId;
+        return this.postService.get(userId);
+    }
+
+    @Get(':id')
+    getById(@Param('id', ParseIntPipe) postId, @GetUser('id') userId: number){
+        return this.postService.getById(userId, postId);
+    }
+
+    @Post()
+    create(@Body() dto: CreatePostDto, @GetUser('id') userId: number){
+        return this.postService.create(userId, dto);
+    }
+
+    @Put(':id')
+    update(@Param('id', ParseIntPipe) postId, @Body() dto: UpdatePostDto, @GetUser('id') userId: number){
+        return this.postService.update(userId, postId, dto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) postId, @GetUser('id') userId: number){
+        return this.postService.remove(userId, postId);
     }
 }
--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePostDto, UpdatePostDto } from './dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(private prisma: PrismaService){}
+
+    async get(userId: number) {
+        return await this.prisma.post.findMany({
+            where: {
+                userId,
+            }
+        })
+    }
+
+    async getById (userId, postId) {
+        const post = await this.prisma.post.findUnique({
+            where: {
+                id: postId,
+            }
+        });
+
+        if(!post || post.userId !== userId) throw new ForbiddenException("Post is not found. Access to resource is denied");
+        return post;
+    }
+
+    async create(userId: number, dto: CreatePostDto){
+        const post = await this.prisma.post.create({
+            data: {
+                userId,
+                ...dto
+            }
+        })
+        return post;
+    }
+
+    async update(userId: number, postId, dto: UpdatePostDto){
+        const post = await this.prisma.post.findUnique({
+            where: {
+                id: postId,
+            }
+        });
+
+        if(!post || post.userId !== userId) throw new ForbiddenException("Post is not found. Access to resource is denied");
+
+        return this.prisma.post.update({
+            where: {
+                id: postId,
+            },
+            data: {
+                ...dto,
+            }
+        });
+    }
+
+    async remove (userId: number, postId: number) {
+        const post = await this.prisma.post.findUnique({
+            where: {
+                id: postId,
+            }
+        });
+
+        if(!post || post.userId !== userId) throw new ForbiddenException("Post is not found. Access to resource is denied");
+
+        await this.prisma.post.delete({
+            where: {
+                id: postId,
+            }
+        });
+
+        return "Post is successfully deleted";
+    }
+}
